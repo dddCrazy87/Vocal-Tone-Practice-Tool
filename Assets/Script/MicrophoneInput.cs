2a81695246eee15c35131cc456141254f633a5d1
Accelerator87 <n126970@gmail.com>
2023-05-02 21:20:17 +0800

    test AudioMixerGroup

--- Assets/Script/MicrophoneInput.cs
+++ Assets/Script/MicrophoneInput.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class MicrophoneInput : MonoBehaviour {
 
     [SerializeField] private AudioSource Audio;
+    public AudioMixerGroup mixerGroup;
     private AudioClip recordedClip;
 
     public void StartRecording() {
         int sampleRate = AudioSettings.outputSampleRate;
         Audio.clip = Microphone.Start(null, true, 10, sampleRate);
         Audio.loop = true;
+        Audio.outputAudioMixerGroup = mixerGroup;
         Audio.Play();
     }
 
